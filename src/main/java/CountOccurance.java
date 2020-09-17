d1ee7788a56be2d1fae190b4b9867771dbc77953
zakykhan <zakykhan47@gmail.com>
2020-09-17 16:51:23 +0530

    zakyPS

--- src/main/java/CountOccurance.java
+++ src/main/java/CountOccurance.java
@@ -1,2 +1,22 @@
+import java.util.HashMap;
+
 public class CountOccurance {
+    public static void CountOccurance(String str){
+        char[] arr = str.toCharArray();
+        HashMap<Character,Integer> countmap = new HashMap<Character, Integer>();
+        for (Character c : arr){
+            if (countmap.containsKey(c)){
+                countmap.put(c,countmap.get(c)+1);
+            }
+            else{
+                countmap.put(c,1);
+            }
+        }
+        System.out.println(str+":----->Occurance: "+countmap);
+    }
+
+    public static void main(String[] args) {
+        String str = "Mohammad zaky Khan";
+        CountOccurance(str);
+    }
 }
