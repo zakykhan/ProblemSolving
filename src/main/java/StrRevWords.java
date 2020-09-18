ab0cfc7962555a8933057fee07d37fdbb6c15d42
zakykhan <zakykhan47@gmail.com>
2020-09-18 15:45:25 +0530

    zakyPS

---
+++ src/main/java/StrRevWords.java
@@ -0,0 +1,15 @@
+public class StrRevWords {
+    static void reverse(String str[]){
+        String ans = " ";
+        for (int i = str.length-1; i >= 0; i--){
+            ans += str[i]+" ";
+        }
+        System.out.println("Reversed Words: ");
+        System.out.println(ans.substring(0)+ans.charAt(0));
+    }
+
+    public static void main(String[] args) {
+        String str[] = " The Name Is Bond James Bond".split(" ");
+        reverse(str);
+    }
+}
