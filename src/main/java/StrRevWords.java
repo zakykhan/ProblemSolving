e78d8201ad14add2ce3da2b29eea06ce09f1b53c
zakykhan <zakykhan47@gmail.com>
2020-09-21 13:45:12 +0530

    zakyPS

--- src/main/java/StrRevWords.java
+++ src/main/java/StrRevWords.java
@@ -1,7 +1,7 @@
 public class StrRevWords {
     static void reverse(String str[]){
         String ans = " ";
-        for (int i = str.length-1; i >= 0; i--){
+        for (int i = str.length-1; i>= 0; i--){
             ans += str[i]+" ";
         }
         System.out.println("Reversed Words: ");
